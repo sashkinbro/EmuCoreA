22d84ab39bf223f5174deeb6e5f0f4d7602cfe90
sashkinbro <sashkinbro@users.noreply.github.com>
2026-09-26 20:40:53 +0300

    Bump version to 0.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "com.sbro.emucorea"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "0.0.3"
+        versionCode = 11
+        versionName = "0.0.4"
 
         buildConfigField("String", "FEEDBACK_ENDPOINT", buildConfigString(feedbackEndpoint))
         buildConfigField("String", "FEEDBACK_API_KEY", buildConfigString(feedbackApiKey))
